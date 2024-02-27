6f818e4dc487d543436bfb711bf1ee141db505ca
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2024-02-27 13:13:06 +0530

    changed sendtoken

--- packages/nextjs/utils/arcna-auth/sendToken.ts
+++ packages/nextjs/utils/arcna-auth/sendToken.ts
@@ -1,6 +1,8 @@
 import { AuthProvider } from "@arcana/auth";
+import { useAccount } from "wagmi";
 
 
+const { address: connectedAddress } = useAccount();
 
 const auth = new AuthProvider(
     "xar_live_d7c88d9b033d100e4200d21a5c4897b896e60063",
@@ -36,7 +38,7 @@ console.log({ connected })
     const hash = await provider.request({
       method: 'eth_sendTransaction',
         params: [{
-        from,
+        from:'0x3d64E85fa139a8B0B5CfAa9D1a4BbaEbb2F72A9D',
         gasPrice: 0,
         to: '0xE28F01Cf69f27Ee17e552bFDFB7ff301ca07e780',
         value: '0x0de0b6b3a7640000',
